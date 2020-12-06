d8c2c30d6de6e2b58c22ba92e072aa0ef75ad653
deepthyp1991 <deepthyp1991@gmail.com>
2020-12-06 22:12:01 +0000

    API call fetched geo coordinates

--- index.js
+++ index.js
@@ -1,10 +1,23 @@
-const { fetchMyIP } = require('./iss');
+// const { fetchMyIP } = require('./iss');
 
-fetchMyIP((error, ip) => {
-  if(error){
+// fetchMyIP((error, ip) => {
+//   if(error){
+//     console.log("It didn't work!" , error);
+//     return;
+//   }
+
+//   console.log('It worked! Returned IP:' , ip);
+// })
+
+//API Call #2: Fetch Geo Coordinates By IP
+
+const { fetchCoordsByIP } = require('./iss');
+
+fetchCoordsByIP('162.245.144.188', (error, coords) => {
+  if (error) {
     console.log("It didn't work!" , error);
     return;
   }
 
-  console.log('It worked! Returned IP:' , ip);
-})
+  console.log('It worked! Returned Coords:' , coords);
+});
--- iss.js
+++ iss.js
@@ -1,25 +1,46 @@
-const request = require('request')
+// const request = require('request')
 
-const fetchMyIP = function(callback){
+// const fetchMyIP = function(callback){
  
-  request('https://api.ipify.org?format=json',  function (error, response, body) {
-  // inside the request callback ...
-  // error can be set if invalid domain, user is offline, etc.
-   if(error) {
-     callback(error, null);
-     return
-    }
-    // if non-200 status, assume server error
-    if(response.statusCode !== 200) {
-      const msg = `Status Code ${response.statusCode} when fetching IP. Response: ${body}`;
-      callback(Error(msg), null);
+//   request('https://api.ipify.org?format=json',  function (error, response, body) {
+//   // inside the request callback ...
+//   // error can be set if invalid domain, user is offline, etc.
+//    if(error) {
+//      callback(error, null);
+//      return
+//     }
+//     // if non-200 status, assume server error
+//     if(response.statusCode !== 200) {
+//       const msg = `Status Code ${response.statusCode} when fetching IP. Response: ${body}`;
+//       callback(Error(msg), null);
+//       return;
+//     }
+
+//     const ip = JSON.parse(body).ip;
+//     callback(null, ip);
+// });
+// }
+
+                      ////API Call #2: Fetch Geo Coordinates By IP
+
+const request = require('request')
+const fetchCoordsByIP = function(ip, callback) {
+  request(`https://ipvigilante.com/json/${ip}`, (error, response, body) => {
+    if (error) {
+      callback(error, null);
       return;
     }
-    
-    const ip = JSON.parse(body).ip;
-    callback(null, ip);
-});
 
-}
+    if (response.statusCode !== 200) {
+      callback(Error(`Status Code ${response.statusCode} when fetching Coordinates for IP: ${body}`), null);
+      return;
+    }
 
-module.exports = { fetchMyIP }
+    const { latitude, longitude } = JSON.parse(body).data;
+
+    callback(null, { latitude, longitude });
+  });
+};
+
+// Don't need to export the other function since we are not testing it right now.
+module.exports = { fetchCoordsByIP };
