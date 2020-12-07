4f75b59e0470a0ab1b0f4c2e5b6e9186038e7d8f
deepthyp1991 <deepthyp1991@gmail.com>
2020-12-07 14:18:37 +0000

    adding files index2 and isspromised, ISS spotter with promises

---
+++ index2.js
@@ -0,0 +1,16 @@
+// const { fetchMyIP,fetchCoordsByIP, fetchISSFlyOverTimes } = require('./iss_promised')
+
+// fetchMyIP()
+// .then(fetchCoordsByIP)
+// .then(fetchISSFlyOverTimes)
+// .then(body => console.log(body))
+
+const { nextISSTimesForMyLocation } = require('./iss_promised');
+
+nextISSTimesForMyLocation()
+  .then((passTimes) => {
+    printPassTimes(passTimes);
+  })
+  .catch((error) => {
+    console.log("It didn't work: ", error.message);
+  });
